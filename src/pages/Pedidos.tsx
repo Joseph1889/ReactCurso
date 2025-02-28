15ed7ae1a43a832874e50a061825894161d2c95a
Joseph1889 <josephh2408@gmail.com>
2025-02-28 16:50:11 -0500

    Add imports to Pedidos.tsx

--- src/pages/Pedidos.tsx
+++ src/pages/Pedidos.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import PageHeader from "../components/PageHeader"
+import { Pedido } from "../types/Pedido";
+import { API_URL } from "../utils";
 
 function Pedidos() {
 
