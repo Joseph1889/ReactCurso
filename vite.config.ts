ebcfba4c613099a2edf763aa086f96d96b77368e
Joseph1889 <josephh2408@gmail.com>
2025-03-01 19:38:38 -0500

    Modify files to build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  base: '/ReactCurso/', // Cambia esto por el nombre de tu repo en GitHub
+  build: {
+    outDir: 'dist'
+  }
 })
